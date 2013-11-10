04262af5c2fbb84b0e54bd059dde4198f3eb6f03
ber5ien <raf@amidev.co.uk>
2013-11-10 09:10:42 +0000

    First Upload

---
+++ javascript/daily_calls_update_v2.js
@@ -0,0 +1,165 @@
+﻿var macro ="" ;
+var i, retcode, errtext;
+var datasource = "C:\\Users\\rzdziech\\Macros\\Datasources\\";
+var rest = "";
+var desc = "";
+var old = "";
+var current_date = "{{!NOW:dd}}/{{!NOW:mm}} - ";
+var text_do_zapisania = "To be chased today";
+var hylas = 3;
+var csv_file_name = "_desc_open_calls.csv";
+var row;
+var wrong_queue = new Array("-","Pre<SP>Scheduling","Scheduling","Installation","Post<SP>Installation","Sales","Contract<SP>Termination","Finance","Dispatch","RMA","Accreditation","Deployment","AUP","Hybeam");
+
+//PREPARE AND DOWNLOAD CSV FILES FOR ITERATION
+
+function calls_setup (hylas,wrong_queue)
+{
+	macro = "CODE:";
+	macro += "URL GOTO=http://oss2.freenoc.lan/OSS/SAP/"+hylas+"/serviceCall\n";
+	//otwieramy servica call w h1 2ndline ktore nie sa zamkniete
+	macro += "TAG POS=1 TYPE=A ATTR=TXT:customise<SP>fields\n";
+	macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-fields-form ATTR=ID:edit-fields CONTENT=%subject:%status:%businessPartner:%createdOn:%description:%queue\n";
+	macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-fields-form ATTR=ID:edit-0\n";
+	macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Column CONTENT=%Status\n";
+	macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Equality CONTENT=%!=\n";
+	macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Value CONTENT=Closed\n";
+	macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-submit\n";
+	//'trzebe dodac 2ndline bo inaczej exportuje wszystkie call zamiast tylko z 2ndline Dev Error :)
+	//macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Column CONTENT=%Queue\n";
+	//macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Value CONTENT=2nd<SP>line\n";
+	//macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-submit\n";
+	for (var b = 0; b < wrong_queue.length; b++)
+	{
+		macro +="TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Column CONTENT=%Queue \n";
+		macro +="TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Equality CONTENT=%!= \n";
+		macro +="TAG POS=1 TYPE=INPUT:TEXT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Value CONTENT="+wrong_queue[b]+"\n";
+		macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-submit\n";
+	}
+}
+
+function save_calls(hylas,csv_file_name)
+{	
+	macro += "URL GOTO=http://oss2.freenoc.lan/OSS/SAP/"+hylas+"/serviceCall\n";
+	//'zapisujemy liste callsow w csv w datasource zebysmy zaraz mogli z niej sobie z czytac i uaktualnic service call-e
+	macro += ("ONDOWNLOAD FOLDER=" + datasource + " FILE="+hylas+csv_file_name+" WAIT=YES\n");
+	macro += "TAG POS=1 TYPE=A ATTR=TXT:export<SP>list\n";
+	
+	//'czekamy 5s zeby csv sie zdarzyl zapisac bo inaczej wywala error 
+	macro += "WAIT SECONDS=5\n";
+}
+
+function macro_run(macro){
+	
+	retcode = iimPlay(macro);
+		
+	if (retcode < 0) {              // an error has occured
+		rerrtext = iimGetLastError();
+		alert(errtext);
+	}
+	//END OF DOWNLOADING CSV FILE
+}
+
+function count_rows(datasource,hylas,csv_file_name)
+{
+	// COUNT THE NUMBERS OF ROWS IN CSV FOR LOOP
+	const CRLF = "\r\n";
+	const LF = "\n";
+	
+	var lines = new Array();               
+	
+	var file_i = imns.FIO.openNode(datasource+hylas+csv_file_name);
+	var text = imns.FIO.readTextFile(file_i);         // Read file into one string
+	
+	// Determine end-of-line marker
+	var eol = (text.indexOf(CRLF) == -1) ? LF : CRLF;
+	
+	// Split into lines (number of lines) NUMBER OF LINES IN CSV
+	lines = text.split(eol);
+	eol = lines.length;
+	//iimDisplay(eol);
+	// FINISH COUNTING EOL = NUmber of LInes 
+	return eol;
+}
+
+function update_descriptions(eol,hylas,csv_file_name,current_date,text_do_zapisania)
+{	
+	// RUN LOOP OVER THE SERVICE CALLS
+	for (var a=2; a < eol ; a++)
+	{
+			iimSet("row",a);
+			
+			//PREPARE MACRO FOR ITERATION
+			macro = "CODE:";
+			macro += "SET !DATASOURCE "+hylas+csv_file_name+"\n";
+			macro += "SET !DATASOURCE_COLUMNS 8\n";
+			
+			//'set up loop start from second row
+			macro += "SET !DATASOURCE_LINE {{row}}\n";
+			macro += "URL GOTO=http://oss2.freenoc.lan/OSS/SAP/"+hylas+"/serviceCall/{{!COL1}}\n";
+			macro += "TAG POS=1 TYPE=A ATTR=TXT:Edit\n";
+			
+			//'pobieramy dane z formularza i zmieniamy date lub dodajemy wpis jak brakuje
+			macro += "TAG POS=1 TYPE=TEXTAREA FORM=ID:OSSMeta-form ATTR=ID:edit-description EXTRACT=TXT";
+			
+			//play macro is needed for iimGetLastExtract function to work properly
+			iimPlay(macro);
+			
+			//iimGetLastExtract zwraca "" jezeli jest puste //wrzucamy zawartosc z description do zmiennej
+			desc = "";
+			old = "";
+			rest = "";
+			desc = iimGetLastExtract();
+			old = iimGetLastExtract();
+			var ind = 0;
+			var i = 0;
+			macro = "CODE:";
+			//sprawdzamy co jest w description
+			if ( desc == "" || desc == "NULL" || desc == " " ) {
+			  // jezeli nic nie ma to dodaj jakis text dzisiajsza date + text 
+				desc = current_date + text_do_zapisania + desc;
+			}else if ( !isNaN(desc[0]) ){
+			 //jezeli pierwszy znak liczba (jakas tam data) to daj nowa i wpisz co bylo poprzednio
+				
+			 	if ( desc.indexOf("-") > 0 )
+			 	{
+			 	 	ind = desc.indexOf("-");
+			 	 	ind += 1;
+			 	}else
+			 	{
+			 		ind = desc.indexOf(" ");
+			 	}
+				for (var i = ind; i < desc.length; i++)
+				{
+				 	rest = rest + desc[i];
+				}
+				macro +="SET !VAR1 \""+ rest + "\"\n";
+				desc = (""+ current_date +"{{!VAR1}}");
+				
+			} else {
+			// jezeli nie mam daty to ja dodaj ja zapisz reszte co bylo wczesniej
+				desc = (""+ current_date + "" + desc + "");
+			}
+			
+			//zapisujemy uaktualnione dane
+			macro += "TAG POS=1 TYPE=TEXTAREA FORM=ID:OSSMeta-form ATTR=ID:edit-description CONTENT=\"" + desc + "\"\n";
+			//macro += "TAG POS=1 TYPE=TEXTAREA FORM=ID:OSSMeta-form ATTR=ID:edit-reason CONTENT=\"Last description: " + old + "\"\n";
+			macro += "TAG POS=1 TYPE=TEXTAREA FORM=ID:OSSMeta-form ATTR=ID:edit-reason CONTENT=\"Description Updated.\"\n";
+			macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-form ATTR=ID:edit-0 \n";
+		macro_run(macro);
+	
+	}
+
+}
+
+calls_setup(1,wrong_queue);
+save_calls(1,csv_file_name);
+save_calls(3,csv_file_name);
+macro_run(macro);
+h1_eol = count_rows(datasource,1,csv_file_name);
+h2_eol = count_rows(datasource,3,csv_file_name);
+update_descriptions(h1_eol,1,csv_file_name,current_date,text_do_zapisania);
+update_descriptions(h2_eol,3,csv_file_name,current_date,text_do_zapisania);
+
+
+
---
+++ javascript/monthly_reports.js
@@ -0,0 +1,139 @@
+﻿// month set up
+var macro = "";
+var retcode;
+var errtext;
+var date = new Date();
+var start_date = "";
+var end_date = "";
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var hylas ="";
+var last_month = 0;
+var datasource_h1 = "h1.csv";
+var datasource_h2 = "h2.csv";
+var datasource = "C:\\Users\\rzdziech\\Macros\\Datasources\\";
+
+if (month == 1)
+{
+    
+   last_month = 12;
+
+}else{
+
+   last_month = month - 1;
+
+}
+
+var download_folder = "ONDOWNLOAD FOLDER=C:\\Users\\rzdziech\\Desktop\\PR<SP>Automation<SP>Project\\datasource\\imacro_export\\{{!NOW:yyyy}}_"+last_month+" FILE={{!COL2}}.csv WAIT=YES \n";
+
+start_date = ""+year+""+last_month+"01";
+end_date = ""+year+""+last_month+"31";
+    
+macro = "CODE:";
+macro += "SET !VAR1 \""+start_date+"\"\n";
+macro += "SET !VAR2 \""+end_date+"\"\n";
+macro += "TAB CLOSEALLOTHERS \n";
+macro += "TAB OPEN \n";
+macro += "TAB T=1 \n";
+macro += "URL GOTO=http://oss2.freenoc.lan/OSS/SAP/3/serviceCall \n";
+
+//form setting up 
+//'TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-reset
+
+//prepare for export
+
+macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Column CONTENT=\%Created<SP>Date\/Time \n";
+macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Equality CONTENT=%> \n";
+macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Value CONTENT={{!VAR1}} \n";
+macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-submit \n";
+macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Column CONTENT=\%Created<SP>Date\/Time \n";
+macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Equality CONTENT=%< \n";
+macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Value CONTENT={{!VAR2}} \n";
+macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-submit \n";
+macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Column CONTENT=%Subject \n";
+macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Equality CONTENT=%!= \n";
+macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Value CONTENT=AUP<SP>Enforcement \n";
+macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-submit \n";
+macro += "TAG POS=1 TYPE=A ATTR=TXT:customise<SP>fields \n";
+macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-fields-form ATTR=ID:edit-fields CONTENT=%subject:%status:%businessPartner:%type:%createdOn:%closedOn:%resolution \n";
+macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-fields-form ATTR=ID:edit-0 \n";
+
+
+retcode = iimPlay(macro);
+	
+if (retcode < 0) {              // an error has occured
+	rerrtext = iimGetLastError();
+	alert(errtext);
+}
+
+// <<<<<<< OSS SETUP HAS FINISHED >>>>>>>>>
+//
+//count the number of partners in csv
+function number_of_lines (datasource, hylas)
+{
+    const CRLF = "\r\n";
+    const LF = "\n";
+
+    var lines = new Array();               
+
+    var file_i = imns.FIO.openNode(datasource+hylas);
+    var text = imns.FIO.readTextFile(file_i);         // Read file into one string
+
+    // Determine end-of-line marker
+    var eol = (text.indexOf(CRLF) == -1) ? LF : CRLF;
+
+    // Split into lines (number of lines) NUMBER OF LINES IN CSV
+    lines = text.split(eol);
+    eol = lines.length;
+    
+    return eol;
+}
+
+// <<<<<<< EXPORTING PARTNERS  >>>>>>>>>
+// loop over the partners
+function loop_over_partners(eol,partner_datasource,download_folder,hylas)
+{
+        
+    // RUN LOOP OVER THE SERVICE CALLS
+    for (var a=2; a < eol ; a++)
+    {
+            iimSet("row",a);
+
+            macro = "CODE:";
+            macro +="SET !DATASOURCE "+partner_datasource+"\n";
+            macro +="SET !DATASOURCE_COLUMNS 8 \n";
+            
+            //set up partner CONTENT=SAP_CODE
+            //'set up loop start from second row
+            macro += "SET !DATASOURCE_LINE {{row}}\n";
+                     
+            macro += "URL GOTO=http://oss2.freenoc.lan/OSS/SAP/"+hylas+"/serviceCall \n";
+            macro += "TAG POS=1 TYPE=SELECT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Column CONTENT=%Business<SP>Partner \n";
+            macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-Value CONTENT={{!COL2}} \n";
+            macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-submit \n";
+            
+            //set up name for files
+            
+            macro += "URL GOTO=http://oss2.freenoc.lan/OSS/SAP/"+hylas+"/serviceCall \n";
+            macro += ""+download_folder+"\n";
+            macro += "TAG POS=1 TYPE=A ATTR=TXT:export<SP>list CONTENT=EVENT:SAVETARGETAS \n";
+            
+            macro += "WAIT SECONDS=2 \n";
+            macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:OSSMeta-filter-form ATTR=ID:edit-undo \n";
+            macro += "WAIT SECONDS=3 \n";
+            
+            
+            retcode = iimPlay(macro);
+                
+            if (retcode < 0) {              // an error has occured
+                rerrtext = iimGetLastError();
+                alert(errtext);
+            }
+    }
+}
+
+eol = number_of_lines(datasource,datasource_h1);
+loop_over_partners(eol,datasource_h1,download_folder,1)
+
+eol = number_of_lines(datasource,datasource_h2);
+loop_over_partners(eol,datasource_h2,download_folder,3)
